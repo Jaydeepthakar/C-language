c9230c8a15282d15b718f6d91acff552d665252f
JaydeepThakar <163496807+Jaydeepthakar@users.noreply.github.com>
2024-03-20 09:42:29 +0530

    labwork 1.2

---
+++ labwork 1.2/JAY.C
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+ clrscr();
+ printf("name:thakar jaydeep amitbhai\n");
+  printf("Age: 19 Year\n");
+   printf("school: Bhagvati Vidhyalay\n\n\n");
+
+  printf("---------\n");
+  printf("|\t|\n");
+  printf("R\t|\n");
+  printf("N\t|\n");
+  printf("W\t|\n");
+  printf("|\t|\n");
+  printf("---------\n\n\n");
+
+    printf("*\n");
+    printf("**\n");
+    printf("***\n");
+    printf("**\n");
+    printf("*\n\n\n");
+     printf("*\n");
+    printf("*\n");
+      printf("*\n");
+	printf("*\t**\t*\n");
+	  printf("*      *  *    *\n");
+	    printf("*     *    *  *\n");
+	      printf("*    *      **\n");
+		printf("*   *\n");
+		  printf("*  *\n");
+			  printf("* *\n");
+			  printf("*");
+getch();
+}
